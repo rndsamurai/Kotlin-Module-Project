cf0e004c6298d63376ad6d10ba42ba4c554b1d86
rostovsamurai <rostovsamurai@yandex.ru>
2025-03-30 00:04:08 +0300

    Проектная работа номер 3.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.Scanner
+
+fun main() {
+    ArchiveList().start()
+    println("Спасибо что воспользовались приложением 'Заметки' :)")
+
 }
